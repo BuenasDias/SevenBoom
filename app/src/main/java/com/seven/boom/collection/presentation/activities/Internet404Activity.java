dd6c850f65364c02e30bbc2f0ce927fa7b27cd7b
Максим Толстобров <maxtah89@gamil.com>
2021-07-17 10:16:41 +0300

    Check

---
+++ app/src/main/java/com/seven/boom/collection/presentation/activities/Internet404Activity.java
@@ -0,0 +1,19 @@
+package com.seven.boom.collection.presentation.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.seven.boom.collection.R;
+
+public class Internet404Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_internet404);
+
+        //TODO реализовать дизайн
+
+    }
+}
---
+++ app/src/main/java/com/seven/boom/collection/utils/InternetConnection.java
@@ -0,0 +1,39 @@
+package com.seven.boom.collection.utils;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+import com.seven.boom.collection.presentation.activities.Internet404Activity;
+
+public class InternetConnection {
+
+    public static void lookError(final Context context){
+        if(!InternetConnection.hasConnection(context)){
+
+            context.startActivity(new Intent(context, Internet404Activity.class));
+        }
+    }
+
+    public static boolean hasConnection(final Context context)
+    {
+        ConnectivityManager cm = (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo wifiInfo = cm.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        if (wifiInfo != null && wifiInfo.isConnected()) {
+            return true;
+        }
+        wifiInfo = cm.getNetworkInfo(ConnectivityManager.TYPE_MOBILE);
+        if (wifiInfo != null && wifiInfo.isConnected()) {
+            return true;
+        }
+        wifiInfo = cm.getActiveNetworkInfo();
+        if (wifiInfo != null && wifiInfo.isConnected()) {
+            return true;
+        }
+        return false;
+    }
+
+
+}

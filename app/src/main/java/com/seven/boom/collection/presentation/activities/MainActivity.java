60e668b1572a080a7c85abd92cd36c0605336c45
Maxim Tolstobrov <tmaksim1989@gmail.com>
2021-07-19 11:33:20 +0300

    check

--- app/src/main/java/com/seven/boom/collection/presentation/activities/MainActivity.java
+++ app/src/main/java/com/seven/boom/collection/presentation/activities/MainActivity.java
@@ -2,11 +2,22 @@ package com.seven.boom.collection.presentation.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+
 import com.seven.boom.collection.R;
+import com.seven.boom.collection.api.network.apiClient.ApiClientMagicChecker;
 import com.seven.boom.collection.api.requests.checker.Response;
 import com.seven.boom.collection.utils.InternetConnection;
 import com.zl.reik.dilatingdotsprogressbar.DilatingDotsProgressBar;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Objects;
+
+import retrofit2.Call;
+import retrofit2.Callback;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,31 +35,32 @@ public class MainActivity extends AppCompatActivity {
 
         InternetConnection.lookError(this);
 
-//        ApiClientMagicChecker.getInstance()
-//                .getApiServiceMagicChecker()
-//                .getCheckerContent()
-//                .enqueue(new Callback<Response>() {
-//                    @Override
-//                    public void onResponse(@NotNull Call<Response> call, retrofit2.@NotNull Response<Response> response) {
-//
-//                        hideProgressBar();
-//
-//                        responseBody = response.body();
-//                        klo = Objects.requireNonNull(responseBody).getContent();
-//
-//                        Log.d("TAG", "Retrofit. klo = " + klo);
-//
-//                        Intent intent = new Intent(MainActivity.this, SlotsActivity.class);
-//                        intent.putExtra("cloaka", klo);
-//
-//                        startActivity(intent);
-//                    }
-//
-//                    @Override
-//                    public void onFailure(@NotNull Call<Response> call, @NotNull Throwable t) {
-//                        t.printStackTrace();
-//                    }
-//                });
+        ApiClientMagicChecker.getInstance()
+                .getApiServiceMagicChecker()
+                .getCheckerContent()
+                .enqueue(new Callback<Response>() {
+                    @Override
+                    public void onResponse(@NotNull Call<Response> call, retrofit2.@NotNull Response<Response> response) {
+
+                        hideProgressBar();
+
+                        responseBody = response.body();
+                        klo = Objects.requireNonNull(responseBody).getContent();
+
+                        Log.d("TAG", "Retrofit. klo = " + klo);
+
+                        Intent intent = new Intent(MainActivity.this, SlotsActivity.class);
+                        intent.putExtra("cloaka", klo);
+
+                        startActivity(intent);
+                        finish();
+                    }
+
+                    @Override
+                    public void onFailure(@NotNull Call<Response> call, @NotNull Throwable t) {
+                        t.printStackTrace();
+                    }
+                });
     }
 
     private void hideProgressBar() {
